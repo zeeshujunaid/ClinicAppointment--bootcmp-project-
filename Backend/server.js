7d840c188f17a667a61fb1ea3ad4bb8824404660
zeeshujunaid <zeeshan.junaid.developer@gmail.com>
2025-11-11 15:08:12 +0500

    fixed

--- Backend/server.js
+++ Backend/server.js
@@ -1,36 +1,33 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./Config/db");
+const authroutes = require("./routes/authRoutes");
+const docroom = require("./routes/roomRoutes");
+const appointmentroutes = require("./routes/appointmentRoutes");
+
 const app = express();
-const connectDB = require('./Config/db');
-const authroutes = require('./routes/authRoutes');
-const docroom = require('./routes/roomRoutes')
-const appointmentroutes = require('./routes/appointmentRoutes');
 
 app.use(express.json());
-
-// Middleware to handle cors
 app.use(cors());
 
-
-connectDB(); 
+connectDB();
 
 app.use("/api/auth", authroutes);
 app.use("/api/appointment", appointmentroutes);
 app.use("/api/room", docroom);
-app.get("/" , (req,res)=>{
-    console.log("hello world")
-})
 
+app.get("/", (req, res) => {
+  console.log("hello world");
+  res.send("Hello World");
+});
 
-const PORT = process.env.PORT || 8080;
+// Local development server
 if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 8080;
   app.listen(PORT, () => {
     console.log(`🚀 Server running locally at: http://localhost:${PORT}`);
   });
 }
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
 
-export default app;
+module.exports = app;
