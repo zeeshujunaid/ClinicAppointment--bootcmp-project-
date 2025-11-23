53fabb70a2f9dfe6f11860b6facdfc986ec7b16c
zeeshujunaid <zeeshan.junaid.developer@gmail.com>
2025-11-23 14:20:42 +0500

    fixing

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 const serverless = require("serverless-http");
-const app = require("../server"); // adjust path if needed
+const app = require("../server");
 
-module.exports.handler = serverless(app);
+module.exports = serverless(app);
